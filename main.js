0e10dbb46d8f6d565b69807ea82342dfeddd9b42
maya-edstrom <medstrom@wisc.edu>
2024-01-29 22:02:47 -0600

    headers

--- main.js
+++ main.js
@@ -1,19 +1,2 @@
 
 
-
-const headers = {
-    'Accept':'application/json'
-  };
-  
-  fetch('https://basemapstyles-api.arcgis.com/arcgis/rest/services/styles/v2/styles/arcgis/terrain',
-  {
-    method: 'GET',
-  
-    headers: headers
-  })
-  .then(function(res) {
-      return res.json();
-  }).then(function(body) {
-      console.log(body);
-  });
-  
